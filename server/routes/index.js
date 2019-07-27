200cb72750125be634caf99df92d0ecdc4fd33d1
noviirna <novi.irnawati@gmail.com>
2019-07-27 17:35:48 +0700

    before deploy

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,10 @@ const routes = require("express").Router();
 const User = require("./user.js");
 const Product = require("./product.js");
 
-routes.use("/", User);
+routes.get("/", function(req, res, next) {
+  res.status(200).json({ message: "server is running" });
+});
+routes.use("/user", User);
 routes.use("/product", Product);
 
 module.exports = routes;
